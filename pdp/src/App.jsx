a6d035c43e1ff543a392e17b742a613eb3e4f2aa
Ayman <ayman.patel97@gmail.com>
2021-11-14 18:51:29 +0530

    feat: Implement Error boundary

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+
+import ErrorBoundary from "./ErrorBoundary";
 
 /* Remote Components from `home` */
 import Footer from "home/Footer";
-import Header from "home/Header";
+const Header = React.lazy(() => import("home/Header"));
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mx-auto max-w-6xl">
-    <Header />
-    <div className="my-10">PDP Hero</div>
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [load, setLoad] = useState(false);
+
+  return (
+    <div className="mx-auto max-w-6xl">
+      {load && (
+        <React.Suspense fallback={<div> Loading ...</div>}>
+          <ErrorBoundary>
+            <Header />
+          </ErrorBoundary>
+        </React.Suspense>
+      )}
+      <button onClick={() => setLoad(true)}>Load External Header</button>
+      <div className="my-10">PDP Hero</div>
+      <Footer />
+    </div>
+  );
+};
+
 ReactDOM.render(<App />, document.getElementById("app"));
